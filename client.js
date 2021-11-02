e8a7bf23d32f1098272500f0c25ad75da6d03f66
AgentChing <ashwin.ragenwar19@vit.edu>
2021-11-02 15:01:37 +0530

    search for something leads to 12 of it's top youtube references, which is then displayed as a thumbnail followed by video title and it's channel

--- client.js
+++ client.js
@@ -1,21 +1,38 @@
-$(document).ready(function(){
-    var API_KEY = "AIzaSyAryMDBFjdBCvp-mzUsHW6ZXFxLulMPGGU"
+$(document).ready(function () {
+  // Api key to access youtube's data from outside.
+  var API_KEY = "AIzaSyAryMDBFjdBCvp-mzUsHW6ZXFxLulMPGGU";
 
-    var video =''
+  //container to store video data which will be displayed once the search is complete
+  var video = "";
 
-    $("form").submit(function(event){
-        event.preventDefault()
-        var search = $("#search").val()
-        videoSearch(API_KEY,search,6)
-    })
+  //on hitting the submit button, this function will be called
+  $("form").submit(function (event) {
+    event.preventDefault();
 
-function videoSearch(key,search,maxResults){
-    $("#videos").empty()
-    $.get("https://www.googleapis.com/youtube/v3/search?key="+key+"&type=video&part=snippet&maxResults="+maxResults+"&q="+search, function(data){
-        console.log(data)
+    //to get value from the search box
+    var search = $("#search").val();
+    //function to get search results
+    videoSearch(API_KEY, search, 12);
+  });
 
-        data.items.forEach(element => {
-            video = `
+
+  function videoSearch(key, search, maxResults) {
+    //Emptying out the previous search result in case there is any
+    $("#videos").empty();
+    
+    //getting and transfering the video elements from youtube data.
+    $.get(
+      "https://www.googleapis.com/youtube/v3/search?key=" +
+        key +
+        "&type=video&part=snippet&maxResults=" +
+        maxResults +
+        "&q=" +
+        search,
+      function (data) {
+        console.log(data);
+
+        data.items.forEach((element) => {
+          video = `
             <a href="https://www.youtube.com/watch?v=${element.id.videoId}">
             <div>
             <iframe width="320" height="180" src="${element.snippet.thumbnails.medium.url}" frameborder="0" allowfullscreen></iframe>
@@ -28,11 +45,10 @@ function videoSearch(key,search,maxResults){
 
             </a>
 
-            `
-            $("#videos").append(video)
+            `;
+          $("#videos").append(video);
         });
-    })
-
-}
-
-})
+      }
+    );
+  }
+});
